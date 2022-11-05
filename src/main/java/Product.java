80a8988192c7dd1369365d49b3d6e5dfd2ee686b
Kentno <kentn@post.ac.il>
2022-11-05 03:02:37 +0200

    Late night bullshittery, @Daniel check Product constructor

--- src/main/java/Product.java
+++ src/main/java/Product.java
@@ -1,10 +1,22 @@
 public class Product {
-    LineItem lnitem = null;
+    //LineItem lnitem = null;
     String id;
     String name;
+    Supplier supplier;
     PremiumAccount premacc;
+    //TODO
+    // Daniel my brother in Christ, I dont think that Product's constructor needs to have lineitem and premaccount
+    // in fact when its created at its most basic it just needs its fields and a Supplier that supplies it
+    // ive created a new constructor and commented yours out, let me know if im the dum and you are the smert
 
-    public Product(LineItem lnitem, String id,String name, PremiumAccount) {
+/*    public Product(LineItem lnitem, String id,String name, PremiumAccount premacc) {
+
+    }*/
+    public Product(String id, String name, Supplier supplier){
+        this.id = id;
+        this.name = name;
+        this.supplier = supplier;
     }
+
 }
 
--- src/main/java/UserInterface.java
+++ src/main/java/UserInterface.java
@@ -3,7 +3,6 @@ import java.util.Vector;
 public class UserInterface {
     static Vector<Account> AccountList = new Vector<>();
     static Vector<Customer> CustomerList = new Vector<>();
-    static Vector<Payment> PaymentList = new Vector<>();
     static Vector<Product> ProductList = new Vector<>();
     static Vector<Supplier> SupplierList = new Vector<>();
     static Vector<User> UserList = new Vector<>();
@@ -11,15 +10,12 @@ public class UserInterface {
     private boolean connected = false;
     private String connected_id;
 
-    public static void main(String[] args) {
-        SupplierList.add(new Supplier("Osem", "Osem"));
-        SupplierList.add(new Supplier("EastWest", "EastWest");
-//        ProductList.add(new Product("Bamba", "Bamba", getSupplier("Osem"))));
 
+    public static void main(String[] args) {
+        PopulateSystem();
     }
 
-
-    private void AddUser(String login_id) {
+    private void AddUser(String login_id){
 
     }
 
@@ -27,24 +23,21 @@ public class UserInterface {
 
     }
 
-
-
     private void LoginUser(String login_id, String password){
-        if (connected)
-        System.out.println("A different user is connected right now."); return;
-        if (getUser(login_id))
-            if (getUser(login_id).password.equals(password)) {
+        if (connected){
+            System.out.println("A different user is connected right now.");
+            return;
+        }
+        User user;
+        if ((user = getUser(login_id)) != null){
+            if (user.password.equals(password)) {
                 connected = true;
                 connected_id = login_id;
                 return;
             }
+        }
         System.out.println("Login details are incorrect.");
     }
-
-    private void CreateOrder(String address){
-
-    }
-
 
     private void CreateOrder(String address){
 
@@ -76,6 +69,49 @@ public class UserInterface {
 
     // Private Methods for Vector Functionality
 
+    /**
+     * @param id ID of the Account
+     * @return The Account with the matching ID if exists, else null
+     */
+    private static Account getAccount(String id){
+        for (Account a : AccountList){
+            if (a.id.equals(id)){
+                return a;
+            }
+        }
+        return null;
+    }
+
+    /**
+     * @param id ID of the Customer
+     * @return The Customer with the matching ID if exists, else null
+     */
+    private static Customer getCustomer(String id){
+        for (Customer c : CustomerList){
+            if (c.id.equals(id)){
+                return c;
+            }
+        }
+        return null;
+    }
+
+    /**
+     * @param id ID of the Product
+     * @return The Product with the matching ID if exists, else null
+     */
+    private static Product getProduct(String id){
+        for (Product p : ProductList){
+            if (p.id.equals(id)){
+                return p;
+            }
+        }
+        return null;
+    }
+
+    /**
+     * @param id ID of the Supplier
+     * @return The Supplier with the matching ID if exists, else null
+     */
     private static Supplier getSupplier(String id){
         for (Supplier s : SupplierList){
             if (s.id.equals(id)){
@@ -84,4 +120,30 @@ public class UserInterface {
         }
         return null;
     }
+
+    /**
+     * @param id ID of the User
+     * @return The User with the matching ID if exists, else null
+     */
+    private static User getUser(String id){
+        for (User u : UserList){
+            if (u.login_id.equals(id)){
+                return u;
+            }
+        }
+        return null;
+    }
+
+    /**
+     * This method is called at the beginning of system runtime to populate the lists with initial entities
+     */
+    private static void PopulateSystem(){
+        SupplierList.add(new Supplier("Osem", "Osem"));
+        SupplierList.add(new Supplier("EastWest", "EastWest"));
+        ProductList.add(new Product("Bamba", "Bamba", getSupplier("Osem")));
+        ProductList.add(new Product("Ramen", "Ramen", getSupplier("EastWest")));
+        //AccountList.add(new Account("Dani", "Dani123", getAccount("EastWest"))); TODO figure out if this needs a Customer to reference
+        // TODO figure out previous thing and by that logic understand how to deal with PremiumAccount
+    }
+
 }
